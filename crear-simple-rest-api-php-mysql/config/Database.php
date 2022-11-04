b181b15fec68ed145437ed87a337c329d61f82a0
Diego <62088939+DiegoMonroy03@users.noreply.github.com>
2022-11-03 23:29:17 -0500

    Update Database.php

--- crear-simple-rest-api-php-mysql/config/Database.php
+++ crear-simple-rest-api-php-mysql/config/Database.php
@@ -1,10 +1,10 @@
 <?php
 class Database{
 
-		private $host  = 'localhost';
-    private $user  = 'root';
-    private $password   = "";
-    private $database  = "baulphp_demo";
+    private $host  = 'ec2-52-5-110-35.compute-1.amazonaws.com';
+    private $user  = 'cdugikbeyrxwjg';
+    private $password   = "b4764ae2f46227168d10f315942244aec75abf4bfc551b788e0f08569e84bd46";
+    private $database  = "d37q2uvtud99o3";
 
     public function getConnection(){
 		$conn = new mysqli($this->host, $this->user, $this->password, $this->database);
